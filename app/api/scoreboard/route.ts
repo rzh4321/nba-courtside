5521c2cd27ecf06e5dc069a31ffeb1392121580a
Ricky <rzh4321@gmail.com>
2023-10-13 21:24:14 -0400

    scoreboard api calls fetch directly instead of calling service

--- app/api/scoreboard/route.ts
+++ app/api/scoreboard/route.ts
@@ -1,7 +1,13 @@
 import { getScoreboard } from '@/service';
 import { NextResponse, NextRequest } from 'next/server';
 
+const host = 'https://cdn.nba.com';
+const scoreboardUrl = `${host}/static/json/liveData/scoreboard/todaysScoreboard_00.json`;
+
+
+
 export async function GET(req: NextRequest) {
-    const data = await getScoreboard();
+    const res = await fetch(scoreboardUrl, { cache: 'no-store' });
+    const data = await res.json();
     return NextResponse.json(data);
 }
